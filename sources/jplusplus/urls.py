ebe2793c13ea237ac028c1c95c121ac5359d9dc4
Edouard <edou4rd@gmail.com>
2014-04-17 16:25:48 +0200

    custom sekizai

--- sources/jplusplus/urls.py
+++ sources/jplusplus/urls.py
@@ -10,9 +10,16 @@
 # Creation : 16-Apr-2014
 # Last mod : 16-Apr-2014
 # -----------------------------------------------------------------------------
-from django.conf.urls import url
+from django.conf.urls import url, include
+from rest_framework import routers
+import api
+
+router = routers.DefaultRouter()
+router.register(r'projects', api.ProjectViewSet)
+router.register(r'offices', api.OfficeViewSet)
 
 urlpatterns = [
+	url(r'^api/v1/', include(router.urls)),
     url(r'^projects/$'                                   , 'jplusplus.views.projects'       , name='jplusplus_projects'),
     url(r'^project/(?P<slug>[\w-]+)/$'                   , 'jplusplus.views.project_details', name='jplusplus_project_details'),
     url(r'^(?P<office>[\w-]+)/projects/$'                , 'jplusplus.views.projects'       , name='jplusplus_projects'),
